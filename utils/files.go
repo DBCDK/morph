bd15d28054211700a2073827f9bef84b86d482fd
Adam Finn Tulinius <atu@dbc.dk>
2018-04-13 13:24:46 +0200

    remove silly debug statement

--- utils/files.go
+++ utils/files.go
@@ -1,13 +1,11 @@
 package utils
 
 import (
-	"fmt"
 	"path/filepath"
 )
 
 func GetAbsPathRelativeTo(path string, reference string) string {
 	if filepath.IsAbs(path) {
-		fmt.Println("abs!")
 		return path
 	} else {
 		return filepath.Join(reference, path)
