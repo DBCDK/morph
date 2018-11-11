9b37448ef65c9c2ac7abe6cb0ea56ac3860181e7
Johan Thomsen <jth@dbc.dk>
2018-11-11 11:06:11 +0100

    Lets include best practise in the examples by pinning the version of nixpkgs

--- examples/secrets.nix
+++ examples/secrets.nix
@@ -1,6 +1,13 @@
+let
+  # Pin the deployment package-set to a specific version of nixpkgs
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/98c1150f2cc62b94b693dce63adc1fbcbfe616f1.tar.gz";
+    sha256 = "1mdwn0qrjc8jli8cbi4cfkar6xq15l232r371p4b48v2d4bah3wp";
+  }) {};
+in
 {
   network =  {
-    pkgs = import <nixpkgs> {};
+    inherit pkgs;
     description = "webserver with secrets";
   };
 
