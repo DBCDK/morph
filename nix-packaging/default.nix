70b92afbe02bea884104461a0d09d326dd6961c3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-03-21 20:45:51 -0700

    fix it

--- nix-packaging/default.nix
+++ nix-packaging/default.nix
@@ -2,24 +2,13 @@
 , version ? "dev"
 }:
 
-with builtins; with lib;
-let
-  blacklistedDirs = [ "nix-packaging" "vendor" "^\\..+$" ];
-  whitelistedFiles = [ "^.+\\.nix$" "^.+\\.go$" "^.+\\.mod$" "^.+\\.sum$"];
-  filterList = file: list: elem true (map (pattern: isList (match pattern file)) list);
-  srcFilter = path: type: (
-    if type == "regular" then filterList (baseNameOf path) whitelistedFiles
-    else if type == "directory" then !filterList (baseNameOf path) blacklistedDirs
-    else false);
-
-in
 buildGoModule rec {
   name = "morph-unstable-${version}";
   inherit version;
 
   buildInputs = [ go-bindata ];
 
-  src = filterSource srcFilter ./..;
+  src = import ./source.nix { inherit lib; };
 
   buildFlagsArray = ''
     -ldflags=
--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   nixpkgsSrc = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/832eb2559d4a4280a0cc3e539080993e121e8d98.tar.gz";
-    sha256 = "01r9w24w34nh90cjanah04fgs9nh87r2jdhmki0zkx2n4ppfnjld";
+    url = "https://github.com/NixOS/nixpkgs/archive/52dae14f0c763dd48572058f0f0906166da14c31.tar.gz";
+    sha256 = "13bnnf4w3jm3cbny2hghrafblbxgxccalc12bpy141vkx2f4qb5a";
   };
 in
 
@@ -25,23 +25,37 @@ let
   makeDeps = writeShellScriptBin "make-deps" ''
     set -e
 
-    # Populate /vendor-dir (for convenience in local dev)
+    export GO111MODULE=on
+
+    # update the modules if requested
     if [ "$1" == "update" ]; then
       ${go}/bin/go get -u
 
+      source="$( nix eval --raw '(with import ${nixpkgsSrc} {}; import ./nix-packaging/source.nix { inherit lib; })' )"
+
       # compute the sha256 of the dependencies
-      GO111MODULE=on GOPATH="$TMPDIR/gopath" ${go}/bin/go mod download
-      sha256="$(${nix}/bin/nix hash-path --base32 "$TMPDIR/gopath/pkg/mod/cache/download" | tr -d '\n')"
+      pushd "$source" >/dev/null
+        export GOPATH="$(mktemp -d)" GOCACHE="$(mktemp -d)"
+        ${go}/bin/go mod download
+        sha256="$( ${nix}/bin/nix hash-path --base32 "$GOPATH/pkg/mod/cache/download" | tr -d '\n' )"
+      popd >/dev/null
+
+      # replace the sha256 in the default.nix
       sed -e "s#modSha256.*#modSha256 = \"$sha256\";#" -i ${packagingOut}/default.nix
-    else
-      ${go}/bin/go mod vendor
+
+      unset GOPATH GOCACHE
     fi
+
+    # Populate /vendor (for convenience in local dev)
+    ${go}/bin/go mod vendor
+
+    unset GO111MODULE
   '';
   makeBuild = writeShellScriptBin "make-build" ''
     set -e
 
-    ${nix}/bin/nix-build -E 'with import ${nixpkgsSrc} {};
-      callPackage ./nix-packaging/default.nix {}' -A out $@
+    ${nix}/bin/nix build '(with import ${nixpkgsSrc} {};
+      callPackage ./nix-packaging/default.nix {})' $@
 
     make-env
   '';
