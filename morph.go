3f90aa885fac1c29fce9242452fa7c0c505744ef
zimbatm <zimbatm@zimbatm.com>
2020-03-31 14:50:51 +0200

    add a SSH_CONFIG_FILE option
    Sometimes it's useful to have a per-project SSH config. That config
    might for example be generated by:
    
        export SSH_CONFIG_FILE=$(direnv_layout_dir)/ssh_config
        gcloud compute config-ssh --ssh-config-file="$SSH_CONFIG_FILE"
    
    This option is a bit of a superset from all the other SSH_* env vars as
    the ssh config file itself allows to set all of those other options.

--- morph.go
+++ morph.go
@@ -408,6 +408,7 @@ func createSSHContext() *ssh.SSHContext {
 		IdentityFile:       os.Getenv("SSH_IDENTITY_FILE"),
 		DefaultUsername:    os.Getenv("SSH_USER"),
 		SkipHostKeyCheck:   os.Getenv("SSH_SKIP_HOST_KEY_CHECK") != "",
+		ConfigFile:         os.Getenv("SSH_CONFIG_FILE"),
 	}
 }
 
