8edd2b53d20c12d9ae4c992b2661a06eacd80918
Christina Sørensen <christina@cafkafk.com>
2024-09-17 07:26:18 +0200

    style: remove deadnix formatter
    Remove deadnix from linters, since it doesn't make sense for morph,
    considering we may have "dead" nix code that's actually used to deploy
    to hosts.
    
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- treefmt.nix
+++ treefmt.nix
@@ -3,7 +3,6 @@
   programs = {
     nixpkgs-fmt.enable = true; # nix formatter
     statix.enable = true; # nix static analysis
-    deadnix.enable = true; # find dead nix code
     shellcheck.enable = true; # bash/shell
     taplo.enable = true; # toml
     yamlfmt.enable = true; # yaml
@@ -13,7 +12,6 @@
     formatter = {
       nixpkgs-fmt.includes = [ "*.nix" "./data/*" ];
       statix.includes = [ "*.nix" "./data/*" ];
-      deadnix.includes = [ "*.nix" "./data/*" ];
     };
   };
 }
