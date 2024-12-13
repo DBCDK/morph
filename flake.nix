037e759bba5a8837b1cf612c8836edff49fc8d6b
Johan Thomsen <joho@nnit.com>
2024-12-13 10:12:47 +0100

    flake: use the package spec in default.nix

--- flake.nix
+++ flake.nix
@@ -78,35 +78,7 @@
 
         packages = rec {
           default = morph;
-          morph = pkgs.buildGoModule rec {
-            name = "morph-unstable-${version}";
-            inherit version;
-
-            src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-
-            ldflags = [
-              "-X main.version=${version}"
-              "-X main.assetRoot=${placeholder "lib"}"
-            ];
-
-            vendorHash = "sha256-Mi0SdvmYao6rLt8+bFcUv2AjHkJTLP85zGka1/cCPzQ=";
-
-            postInstall = ''
-              mkdir -p $lib
-              cp -v ./data/*.nix $lib
-            '';
-
-            outputs = [
-              "out"
-              "lib"
-            ];
-
-            meta = {
-              homepage = "https://github.com/DBCDK/morph";
-              description = "Morph is a NixOS host manager written in Golang.";
-              mainProgram = "morph";
-            };
-          };
+          morph = pkgs.callPackage ./default.nix { inherit version; };
         };
       }
     );
