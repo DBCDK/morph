8fd6808afdd725ddba51a97fe01479aed0e480b1
Adam F. Tulinius <adtu@dbc.dk>
2025-05-07 12:43:56 +0200

    nixpkgs: nixos-24.11 -> nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flake-utils = {
       url = "github:numtide/flake-utils";
